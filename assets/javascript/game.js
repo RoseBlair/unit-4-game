cf7754dfc808ec1e8ba3c2d7a2bd45dc31addcc7
Carolyn Bernier <carolynbbernier@gmail.com>
2018-09-09 17:32:15 -0400

    added javascript for counter, number to hit, and crystal values

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+
+
+
+
+  
+
+  $(document).ready(function() {
+
+var numberToHit = 0;
+var counter = 0;
+
+$("#counter").text(counter);
+
+numberToHit = Math.floor(Math.random() * 100) +19;
+$("#numberToHit").text(numberToHit);
+
+var crystal1 = Math.floor(Math.random() * 11) +1;
+$("#crystal1").text(crystal1);
+
+var crystal2 = Math.floor(Math.random() * 11) +1;
+$("#crystal2").text(crystal2);
+
+var crystal3 = Math.floor(Math.random() * 11) +1;
+$("#crystal3").text(crystal3);
+
+var crystal4 = Math.floor(Math.random() * 11) +1;
+$("#crystal4").text(crystal4);
+
+
+// (".button").on("click", function() {
+
+    
+// });
+
+
+
+  });
+
