d76e703b896d6b73a8d844b4c9a1d5457855b741
Carolyn Bernier <carolynbbernier@gmail.com>
2018-09-09 17:56:31 -0400

    made crystals equal to random values, made counter run up in console, attempted to make a reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,35 +3,58 @@
 
 
   
-
-  $(document).ready(function() {
+$(document).ready(function() {
 
 var numberToHit = 0;
 var counter = 0;
 
 $("#counter").text(counter);
 
+function initializeCounter() {
+    counter = 0;
+};
+
 numberToHit = Math.floor(Math.random() * 100) +19;
 $("#numberToHit").text(numberToHit);
 
 var crystal1 = Math.floor(Math.random() * 11) +1;
-$("#crystal1").text(crystal1);
+
 
 var crystal2 = Math.floor(Math.random() * 11) +1;
-$("#crystal2").text(crystal2);
+
 
 var crystal3 = Math.floor(Math.random() * 11) +1;
-$("#crystal3").text(crystal3);
+
 
 var crystal4 = Math.floor(Math.random() * 11) +1;
-$("#crystal4").text(crystal4);
 
 
-// (".button").on("click", function() {
 
-    
-// });
+$("#crystal1").on("click", function() {
+    counter = counter + crystal1;
+    console.log(counter);
+});
 
+$("#crystal2").on("click", function() {
+    counter = counter + crystal2;
+    console.log(counter);
+});
+
+$("#crystal3").on("click", function() {
+    counter = counter + crystal3;
+    console.log(counter);
+});
+
+$("#crystal4").on("click", function() {
+    counter = counter + crystal4;
+    console.log(counter);
+});
+
+
+
+if (counter=== numberToHit || counter > numberToHit) {
+    initializeCounter();
+};
 
 
   });
